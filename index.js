8d24d9715125f866265b772a1718661330430fbb
rohit verma <rohithverma.dev@gmail.com>
2024-07-20 18:17:19 +0530

    node-express-vercel

---
+++ index.js
@@ -0,0 +1,15 @@
+// Import packages
+const express = require("express");
+const home = require("./routes/home");
+require("dotenv").config()
+
+// Middlewares
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use("/home", home);
+
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
